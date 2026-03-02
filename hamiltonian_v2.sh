955453194bd48cf34f08d26546bf5f56ec072ab5
perrygliu-svg <perrygliu@g.ucla.edu>
2026-03-02 12:04:12 -0800

    update new hamiltonian file with modified prior

---
+++ hamiltonian_v2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o $HOME/joblogs/joblog.$JOB_ID
+#$ -j y
+#$ -l highp,h_rt=23:45:00
+#$ -pe shared 1
+#$ -t 1-1:1
+#$ -M $USER@mail
+#$ -m bea
+
+BEAST_LOCATION="/u/home/p/perry/Projects/beast-mcmc" # with the path to the location
+xml_analysis="/u/home/p/perry/phylogenetic/RABV_hamiltonian_v2.xml" #including both the path and the name
+BEAGLE_ORDER="0"
+
+. /u/local/Modules/default/init/modules.sh
+module load gcc
+module load amd/rocm
+
+export GPU_DEVICE_ORDINAL=$SGE_HGR_vega
+echo "Job $JOB_ID.$SGE_TASK_ID started on:   " `hostname -s`
+echo "Date: " `date `
+echo "Device: $GPU_DEVICE_ORDINAL" # TODO check if this is correct since the output is 0
+echo "Script name $0"
+
+export LD_LIBRARY_PATH="$HOME/lib:$\{LD_LIBRARY_PATH:-\}"
+export PKG_CONFIG_PATH="$HOME/lib/pkgconfig:$\{PKG_CONFIG_PATH:-\}"
+
+echo "The output folder is: ${folder_name}"
+echo "Running Analysis on Beast... "
+    java -jar "${BEAST_LOCATION}/build/dist/beast.jar" -beagle_order $BEAGLE_ORDER \
+    -seed 666 -overwrite "${xml_analysis}" > "control.txt" 2>&1
+
